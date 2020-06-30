df8a6e3f621459a44f8d061165a42e7450c98530
Jason <66390761+echefcy@users.noreply.github.com>
2020-06-30 19:12:16 -0400

    Update pos.py

--- pos.py
+++ pos.py
@@ -1,9 +1,35 @@
 import matrix
 import quaternion
 
-# transformation matrices
-
 class Position:
+
+    class VCam:
+        # virtual camera - an abstract model of a camera that contains
+        #                  information about the camera's position and
+        #                  orientation
+        # all Positions share a single camera
+
+        def __init__(self, p = [0, 0, 0], uv = [0, 1, 0]):
+            self.pos = p
+            self.upvec = uv
+        
+        def rotateX(self, theta):
+            p = quaternion.rotate(self.pos, [1,0,0], theta)
+            uv = quaternion.rotate(self.upvec, [1,0,0], theta)
+            return Position.VCam(p, uv)
+
+        def rotateY(self, theta):
+            p = quaternion.rotate(self.pos, [0,1,0], theta)
+            uv = quaternion.rotate(self.upvec, [0,1,0], theta)
+            return Position.VCam(p, uv)
+
+        def rotateZ(self, theta):
+            p = quaternion.rotate(self.pos, [0,0,1], theta)
+            uv = quaternion.rotate(self.upvec, [0,0,1], theta)
+            return Position.VCam(p, uv)
+
+        def translate(self, x, y, z):
+            return Position.VCam([self.pos[0] + x, self.pos[1] - y, self.pos[2] + z], self.upvec)
 
     def __init__(self, x, y, z):
         self.vec = [
@@ -63,24 +89,30 @@ class Position:
 
     # transform world space coords into eye space coords (view matrix)
     # it's inverse of the camera's transformation matrix in world-space
-    def cam_transform(self, cam_pos, cam_upvec = [[0],[1],[0]]):
-        """cam_pos = column vec3 representing camera's position
-        cam_upvec = column vec3 representing camera's upright direction
+    def cam_transform(self, camera):
+        """camera = VCam object
         returns Position object representing the camera's view"""
 
-        t = self.translate(-cam_pos[0][0], -cam_pos[1][0], -cam_pos[2][0])
+        # translation inversion
+        t = self.translate(-camera.pos[0], -camera.pos[1], -camera.pos[2])
+        cam_pos_col = [[camera.pos[0]], [camera.pos[1]], [camera.pos[2]]]
+        cam_upvec_col = [[camera.upvec[0]], [camera.upvec[1]], [camera.upvec[2]]]
 
+        # rotation inversion
         # ***maybe try quaternions here instead?
         # 3 perpendicular unit vecs with Z pointing towards the camera
-        Z = matrix.normalize(matrix.subtract(t.vec[0:3], cam_pos))
-        X = matrix.normalize(matrix.crossprod(cam_upvec, Z))
+        Z = matrix.normalize(matrix.subtract(t.vec[0:3], cam_pos_col))
+        X = matrix.normalize(matrix.crossprod(cam_upvec_col, Z))
         Y = matrix.crossprod(Z, X)
+
+        # camera rotation inversion matrix for the object  
         C = [
             [X[0][0], X[1][0], X[2][0], 0], # [Xaxis.x, Xaxis.y, Xaxis.z, 0]
             [Y[0][0], Y[1][0], Y[2][0], 0], # [Yaxis.x, Yaxis.y, Yaxis.z, 0]
             [Z[0][0], Z[1][0], Z[2][0], 0], # [Zaxis.x, Zaxis.y, Zaxis.z, 0]
             [0, 0, 0, 1]
         ]
+        # the camera transformation can be done with a single matrix mult
         vec = matrix.multiply(C, t.vec)
         return Position(vec[0][0],vec[1][0],vec[2][0])
 
@@ -136,25 +168,6 @@ class Position:
         # ***work on adding Z mapping
         return [(width/2)*vec[0][0] + x_offset + width/2,
             (height/2)*vec[1][0] + y_offset + height/2]
-    
-if __name__ == "__main__":
-    # v = Position(1,1,1)
-    # # top 4 vertex
-    # v1 = Position(1,1,1)
-    # v2 = Position(-1,1,1)
-    # v3 = Position(-1,1,-1)
-    # v4 = Position(1,1,-1)
-    # # bottom 4 vertex
-    # v5 = Position(1,-1,1)
-    # v6 = Position(-1,-1,1)
-    # v7 = Position(-1,-1,-1)
-    # v8 = Position(1,-1,-1)
-    # vertex = [v1,v2,v3,v4,v5,v6,v7,v8]
 
-    # for v in range(len(vertex)):
-    #     vertex[v].project(l=-2,r=2,t=2,b=-2,n=2,f=8)
-    
-    # print("\n\n")
-    # for v in range(len(vertex)):
-    #     print(vertex[v].vec)
+if __name__ == "__main__":
     pass
